6759136f1bd4f72f7f19782ab28618e783fa4735
Garf Dang <garfdang@duck.com>
2024-01-23 23:41:18 -0500

    [fix] change leap hotkey to C-j

--- lua/garf/plugins/leap.lua
+++ lua/garf/plugins/leap.lua
@@ -2,6 +2,11 @@ return {
     'ggandor/leap.nvim',
     dependencies = { "tpope/vim-repeat" },
     config = function ()
-        require('leap').create_default_mappings()
+        -- require('leap').create_default_mappings()
+        -- vim.keymap.set({'n','x'}, '<CR>', function ()
+        vim.keymap.set({'n','x'}, '<C-j>', function ()
+            local current_window = vim.fn.win_getid()
+            require('leap').leap { target_windows = { current_window } }
+        end)
     end,
 }
