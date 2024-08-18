2e704497eb862d62d7cc09602a67250b24e9e2d1
Garf Dang <garfdang@duck.com>
2024-08-18 16:47:32 -0400

    [chn] aerial settings
    add nav modee
    change maximum width

--- lua/garf/plugins/sidebar/aerial.lua
+++ lua/garf/plugins/sidebar/aerial.lua
@@ -15,9 +15,20 @@ return {
                 vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
                 vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
             end,
+
+            layout = {
+                -- These control the width of the aerial window.
+                -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+                -- min_width and max_width can be a list of mixed types.
+                -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
+                max_width = { 0.5 },
+                width = nil,
+                min_width = 20,
+            },
         })
         -- You probably also want to set a keymap to toggle aerial
         vim.keymap.set("n", "<leader>et", "<cmd>AerialToggle!<CR>")
+        vim.keymap.set("n", "<leader>en", "<cmd>AerialNavToggle<CR>")
 
         local lualine = require("lualine")
         lualine.setup({
